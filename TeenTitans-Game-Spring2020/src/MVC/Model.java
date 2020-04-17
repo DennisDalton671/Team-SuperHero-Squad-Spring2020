6ef94ea5158869063c6cec52d90dba163c6343fb
Taylor Williams <twilli21@ggc.edu>
2020-04-17 01:09:57 -0400

    Fix parsing from database error in Model.java

--- TeenTitans-Game-Spring2020/src/MVC/Model.java
+++ TeenTitans-Game-Spring2020/src/MVC/Model.java
@@ -55,22 +55,21 @@ public class Model {
             while (rs.next()) {
                 String puzzle_id = rs.getString(1);
                 String puzzle_desc = rs.getString(2);
-                String monster_id = rs.getString(3);
-                String puzzle_desc2 = rs.getString(4);
-                String puzzle_desc3 = rs.getString(5);
-                String puzzle_desc4 = rs.getString(6);
-                String hint = rs.getString(7);
-                String hint2 = rs.getString(8);
-                String hint3 = rs.getString(9);
-                String hint4 = rs.getString(10);
-                String solution = rs.getString(11);
-                String solution2 = rs.getString(12);
-                String solution3 = rs.getString(13);
-                String solution4 = rs.getString(14);
-                String reward = rs.getString(15);
-                String penalty = rs.getString(16);
-                String room_puzzle = rs.getString(17);
-                String room_puzzle2 = rs.getString(18);
+                String puzzle_desc2 = rs.getString(3);
+                String puzzle_desc3 = rs.getString(4);
+                String puzzle_desc4 = rs.getString(5);
+                String hint = rs.getString(6);
+                String hint2 = rs.getString(7);
+                String hint3 = rs.getString(8);
+                String hint4 = rs.getString(9);
+                String solution = rs.getString(10);
+                String solution2 = rs.getString(11);
+                String solution3 = rs.getString(12);
+                String solution4 = rs.getString(13);
+                String reward = rs.getString(14);
+                String penalty = rs.getString(15);
+                String room_puzzle = rs.getString(16);
+                String room_puzzle2 = rs.getString(17);
 
                 plist.add(new Puzzle(puzzle_id, puzzle_desc, puzzle_desc2, puzzle_desc3, puzzle_desc4, hint, hint2, hint3, hint4, solution, solution2, solution3, solution4, reward, penalty, room_puzzle, room_puzzle2));
 
@@ -89,7 +88,7 @@ public class Model {
         try {
             Connection con = DriverManager.getConnection(url);
             Statement s = con.createStatement();
-            ResultSet rs = s.executeQuery("SELECT monster_id, monster, monster_desc, health_point, attack_point, room_id FROM monsters");
+            ResultSet rs = s.executeQuery("SELECT monster_id, monster, monster_desc, health_point, attack_point, room_id, defeat_message, item_reward, item_reward2 FROM monsters");
             while (rs.next()) {
                 String monster_id = rs.getString(1);
                 String monster = rs.getString(2);
@@ -100,7 +99,7 @@ public class Model {
                 String defeat_message = rs.getString(7);
                 String item_reward = rs.getString(8);
                 String item_reward2 = rs.getString(9);
-
+                System.out.println(rs.getString(1) + "\t\t\t" + rs.getString(2) + "\t\t\t" + rs.getString(3) + "\t\t\t" + rs.getString(4) + "\t\t\t" + rs.getString(5) + "\t\t\t" + rs.getString(6) + "\t\t\t" + rs.getString(7) + "\t\t\t" + rs.getString(8) + "\t\t\t" + rs.getString(9));
                 mlist.add(new Monster(monster_id, monster, monster_desc, health_point, attack_point, room_id, defeat_message, item_reward, item_reward2));
 
             }
@@ -116,7 +115,7 @@ public class Model {
         try {
             Connection con = DriverManager.getConnection(url);
             Statement s = con.createStatement();
-            ResultSet rs = s.executeQuery("SELECT item_id, item, item_desc, acquired, acquired_additional, item_boost, item_benefit, item_use, craft FROM monsters");
+            ResultSet rs = s.executeQuery("SELECT item_id, item, item_desc, acquired, acquired_additional, item_boost, item_benefit, item_use, Craft FROM items");
             while (rs.next()) {
                 String item_id = rs.getString(1);
                 String item = rs.getString(2);
@@ -127,7 +126,7 @@ public class Model {
                 String item_benefit = rs.getString(7);
                 String item_use = rs.getString(8);
                 String craft = rs.getString(9);
-
+                System.out.println(rs.getString(1) + "\t\t\t" + rs.getString(2) + "\t\t\t" + rs.getString(3) + "\t\t\t" + rs.getString(4) + "\t\t\t" + rs.getString(5) + "\t\t\t" + rs.getString(6) + "\t\t\t" + rs.getString(7) + "\t\t\t" + rs.getString(8) + "\t\t\t" + rs.getString(9));
                 ilist.add(new Item(item_id, item, item_desc, acquired, acquired_additional, item_boost, item_benefit, item_use, craft));
 
             }
